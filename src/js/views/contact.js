c98d0be151a2c86307b88b92b04ebe9f393a4e45
Guillermo C.G <151522926+GuilleMarejada@users.noreply.github.com>
2024-06-16 17:19:37 +0000

    actualizamos import navbar

--- src/js/views/contact.js
+++ src/js/views/contact.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ContactDisplay from "../component/contactCard";
 import NavBar from "../component/navBar";
-
 export const Contact = () => {
   return (
     <div className="container">
